fcd51ccdef2bd6707c070904e1f08905df8b2bd7
Code-X <andrewomosh44@gmail.com>
2023-05-19 20:59:37 +0300

    build: add railway config

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,17 +1,33 @@
+// module.exports = ({ env }) => ({
+//     connection: {
+//       client: 'postgres',
+//       connection: {
+//         host: env('DATABASE_HOST', '127.0.0.1'),
+//         port: env.int('DATABASE_PORT', 5432),
+//         database: env('DATABASE_NAME', 'strapi'),
+//         user: env('DATABASE_USERNAME', 'user'),
+//         password: env('DATABASE_PASSWORD', '1234  '),
+//         schema: env('DATABASE_SCHEMA', 'public'), // Not required
+//         ssl: {
+//             ca: env('DATABASE_SSL_CA')
+//         },
+//       },
+//       debug: false,
+//     },
+//   });
+
+
+// railway-config
 module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
     connection: {
-      client: 'postgres',
-      connection: {
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        schema: env('DATABASE_SCHEMA', 'public'), // Not required
-        ssl: {
-            ca: env('DATABASE_SSL_CA')
-        },
-      },
-      debug: false,
+      host: env('PGHOST', '127.0.0.1'),
+      port: env.int('PGPORT', 5931),
+      database: env('PGDATABASE', 'railway'),
+      user: env('PGUSER', 'postgres'),
+      password: env('PGPASSWORD', 'password'),
+      ssl: env.bool(true),
     },
-  });
+  },
+});
